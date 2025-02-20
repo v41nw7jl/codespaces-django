1ab4368cccd7a7009e9649323c8729ee3715070d
v41nw7jl <167311253+v41nw7jl@users.noreply.github.com>
2025-02-20 13:32:57 +0000

    changes

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hello_world project.
+URL configuration for todolist project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,17 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-from hello_world.core import views as core_views
+from django.urls import path
 
 urlpatterns = [
-    path("", core_views.index),
     path("admin/", admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
